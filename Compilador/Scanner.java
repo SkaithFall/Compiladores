5928a9ba88cfd640248c368851123777db3bd663
Aldo Miranda <aldoskaith_19@outlook.com>
2023-05-30 21:31:05 -0600

    parser 1.2.1

--- Compilador/Scanner.java
+++ Compilador/Scanner.java
@@ -10,12 +10,22 @@ public class Scanner {
 
     private final List<Token> tokens = new ArrayList<>();
 
-    private static final Map<String, Tokentype> palabrasReservadas;
+    private static final Map<String, Tokentype> keywords;
     static {
-        palabrasReservadas = new HashMap<>();
-        palabrasReservadas.put("super", Tokentype.SUPER);
-        palabrasReservadas.put("true", Tokentype.TRUE);
-        palabrasReservadas.put("false", Tokentype.FALSE);
+        keywords = new HashMap<>();
+        keywords.put("super", Tokentype.SUPER);
+        keywords.put("true", Tokentype.TRUE);
+        keywords.put("false", Tokentype.FALSE);
+        keywords.put("null", Tokentype.NULL);
+        keywords.put("this", Tokentype.THIS);
+        keywords.put("and", Tokentype.AND);
+        keywords.put("or", Tokentype.OR);
+        keywords.put("while", Tokentype.WHILE);
+        keywords.put("else", Tokentype.ELSE);
+        keywords.put("return", Tokentype.RETURN);
+        keywords.put("print", Tokentype.PRINT);
+        keywords.put("for", Tokentype.FOR);
+        keywords.put("fun", Tokentype.FUN);
     }
 
     Scanner(String source){
@@ -30,11 +40,81 @@ public class Scanner {
 
         for(int i=0; i<source.length(); i++){
             caracter = source.charAt(i);
-
+            char nxtcaracter = (i + 1 < source.length()) ? source.charAt(i + 1) : '\0';
+            
             switch (estado){
                 case 0:
-                    if(caracter == '*'){
-                        tokens.add(new Token(Tokentype.MULTI, "*", i + 1));
+                    if(caracter == '+'){
+                        tokens.add(new Token(Tokentype.SUMA, "+", i + 1));
+                    }
+                    else if(caracter == '-')
+                    {
+                        tokens.add(new Token(Tokentype.RESTA, "-", i + 1));
+                    }
+                    else if(caracter == '*')
+                    {
+                    tokens.add(new Token(Tokentype.MULTI, "*", i + 1));
+                    }
+                    else if(caracter == '/')
+                    {
+                    tokens.add(new Token(Tokentype.DIV, "/",i + 1));
+                    }
+                    else if(caracter == '=')
+                    {
+                        if(nxtcaracter == '=')
+                        {
+                            tokens.add(new Token(Tokentype.ASIG, "==", i + 1));
+                            i++;
+                        }
+                        else
+                        {
+                            tokens.add(new Token(Tokentype.IGUAL, "=", i + 1));
+                        }
+                    }
+                    else if(caracter == '(')
+                    {
+                        tokens.add(new Token(Tokentype.PAR_ABRE, "(", i + 1));
+                    }
+                    else if(caracter == ')')
+                    {
+                        tokens.add(new Token(Tokentype.PAR_CIERRE, ")", i + 1));
+                    }
+                    else if(caracter == '{')
+                    {
+                        tokens.add(new Token(Tokentype.LLAV_ABRE, "{", i + 1));
+                    }
+                    else if(caracter == '}')
+                    {
+                        tokens.add(new Token(Tokentype.LLAV_CIERRE, "}", i + 1));
+                    } 
+                    else if(caracter == '<')
+                    {
+                        if(nxtcaracter == '=')
+                        {
+                            tokens.add(new Token(Tokentype.MENOR_IGUAL, "<=", i + 1));
+                            i++;
+                        }
+                        else
+                        {
+                            tokens.add(new Token(Tokentype.MENOR, "<", i + 1));
+                        }
+                    }
+                    else if(caracter == '>')
+                    {
+                        if(nxtcaracter == '=')
+                        {
+                            tokens.add(new Token(Tokentype.MENOR_IGUAL, "<=", i + 1));
+                            i++;
+                        }
+                        else
+                        {
+                            tokens.add(new Token(Tokentype.MAYOR, ">", i + 1));
+                        }
+                    }
+                    else if(caracter == '!' && nxtcaracter == '=')
+                    {
+                        tokens.add(new Token(Tokentype.DIFERENTE, "!=", i + 1));
+                        i++;
                     }
                     else if(caracter == ','){
                         tokens.add(new Token(Tokentype.COMA, ",", i + 1));
@@ -54,7 +134,7 @@ public class Scanner {
                         lexema = lexema + caracter;
                     }
                     else{
-                        Tokentype tt = palabrasReservadas.get(lexema);
+                        Tokentype tt = keywords.get(lexema);
                         if(tt == null){
                             tokens.add(new Token(Tokentype.ID, lexema, inicioLexema + 1));
                         }
